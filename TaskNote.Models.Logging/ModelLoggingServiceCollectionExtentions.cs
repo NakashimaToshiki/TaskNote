85b674be18e0c3c66db7244b7ec1e56d6a18cf68
Nakashima Toshiki <nakashima.toshiki.app@gmail.com>
2021-06-16 21:50:53 +0900

    ModelsLoggingプロジェクトの追加

---
+++ TaskNote.Models.Logging/ModelLoggingServiceCollectionExtentions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using TaskNote.Logging;
+using TaskNote.Models.Repositoris;
+
+namespace TaskNote.Models.Logging
+{
+    public static class ModelLoggingServiceCollectionExtentions
+    {
+        public static IServiceCollection AddLogging(this IServiceCollection services, LoggingOptions options)
+        {
+            services.AddTaskNoteLogging(options);
+
+            //　必要があればここでクラスごとでログの取得方法を変更する
+
+            return services;
+        }
+    }
+}
