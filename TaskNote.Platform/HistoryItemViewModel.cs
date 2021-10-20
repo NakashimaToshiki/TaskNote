a510b1481f47f77bdeec49b8dba2ba9733ba1c01
NakashimaToshiki <80368114+NakashimaToshiki@users.noreply.github.com>
2021-10-20 12:31:18 +0900

    Xamarinプロジェクトの削除

---
+++ TaskNote.Platform/HistoryItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace TaskNote.Platform
+{
+    /// <summary>
+    /// 検索履歴のViewModel
+    /// </summary>
+    public class HistoryItemViewModel : BindableBase
+    {
+
+        private string _filterRuleText;
+
+        public string FilterRuleText
+        {
+            get => _filterRuleText;
+            set
+            {
+                _filterRuleText = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
