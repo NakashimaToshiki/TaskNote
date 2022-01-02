0c74cf2beafc96015e9ad012ef78a897e72ce0b1
NakashimaToshiki <80368114+NakashimaToshiki@users.noreply.github.com>
2022-01-02 13:43:03 +0900

    task-list.jsの修正

--- TaskNote.JQruery/wwwroot/js/task-list.js
+++ TaskNote.JQruery/wwwroot/js/task-list.js
@@ -2,5 +2,12 @@
 
 function onAddTask() {
     let api = new ApiTask();
-    api.post();
+    //api.post();
+
+    //let ul = $('#ul');
+    var ul = document.getElementById('ul');
+    var li = document.createElement('li');
+    var text = document.createTextNode('abc');
+    li.appendChild(text);
+    ul.appendChild(li);
 }
