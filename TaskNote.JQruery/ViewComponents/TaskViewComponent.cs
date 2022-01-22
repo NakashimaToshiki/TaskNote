d4c0bfe0af94b20df9e268fc74415937ce557f3a
NakashimaToshiki <80368114+NakashimaToshiki@users.noreply.github.com>
2022-01-22 15:26:26 +0900

    ポスト通信サンプル実装

---
+++ TaskNote.JQruery/ViewComponents/TaskViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskNote.Entity.Sessions;
+
+namespace TaskNote.JQruery.ViewComponents
+{
+    public enum TaskViewComponentType
+    {
+        Default,
+    }
+
+    public class TaskViewComponent : ViewComponent
+    {
+        private readonly ITaskSession _session;
+
+        public TaskViewComponent(ITaskSession session)
+        {
+            _session = session ?? throw new ArgumentNullException(nameof(session));
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string prefix, int id, TaskViewComponentType viewType)
+        {
+            var item = await _session.GetByIdAsync(id);
+            return View(viewType.ToString(), item);
+        }
+    }
+}
